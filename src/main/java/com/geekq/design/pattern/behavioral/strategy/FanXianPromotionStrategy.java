b40cfdab7c951db2e3826cfb35f7bb7fefd7733b
qiurunze123 <QiuRunZe_key@163.com>
2018-10-18 00:03:52 +0800

    smartgit -- go

--- src/main/java/com/geekq/design/pattern/behavioral/strategy/FanXianPromotionStrategy.java
+++ src/main/java/com/geekq/design/pattern/behavioral/strategy/FanXianPromotionStrategy.java
@@ -7,5 +7,6 @@ public class FanXianPromotionStrategy implements PromotionStrategy{
     @Override
     public void doPromotion() {
         System.out.println("返现促销,返回的金额存放到用户的余额中");
+        System.out.println("返现促销,返回的金额存放到用户的余额中");
     }
 }
