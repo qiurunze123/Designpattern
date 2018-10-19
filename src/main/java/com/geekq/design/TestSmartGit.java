341c67ccd2ea07d6b2d33b7818d32d86547186cb
qiurunze <qiurunze@youxin.com>
2018-10-19 10:14:04 +0800

    smartgit test

--- src/main/java/com/geekq/design/TestSmartGit.java
+++ src/main/java/com/geekq/design/TestSmartGit.java
@@ -5,5 +5,6 @@ public class TestSmartGit {
     public static void main(String[] args) {
         System.out.println("如果感到快乐你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
+//        System.out.println("如果感到得瑟你就拍拍手！/**/！");
     }
 }
