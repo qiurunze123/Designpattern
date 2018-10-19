bdad3793c35cdc71494a5a8c6ec5349414df56b0
qiurunze <qiurunze@youxin.com>
2018-10-19 10:40:33 +0800

    smartgit  stash ok!

--- src/main/java/com/geekq/design/TestSmartGit.java
+++ src/main/java/com/geekq/design/TestSmartGit.java
@@ -5,6 +5,9 @@ public class TestSmartGit {
     public static void main(String[] args) {
         System.out.println("如果感到快乐你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
-//        System.out.println("如果感到得瑟你就拍拍手！/**/！");
+        System.out.println("如果感到幸福你就拍拍手！！");
+        System.out.println("如果感到幸福你就拍拍手！！");
+        System.out.println("如果感到幸福你就拍拍手！！");
+        System.out.println("如果感到得瑟你就拍拍手！/**/！");
     }
 }
