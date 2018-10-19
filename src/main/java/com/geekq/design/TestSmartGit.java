f517fa0cc84fda1d7289838e36bb71efd1371387
qiurunze <qiurunze@youxin.com>
2018-10-19 10:42:05 +0800

    测试 冲突解决！！

--- src/main/java/com/geekq/design/TestSmartGit.java
+++ src/main/java/com/geekq/design/TestSmartGit.java
@@ -4,6 +4,10 @@ public class TestSmartGit {
 
     public static void main(String[] args) {
         System.out.println("如果感到快乐你就拍拍手！！");
+        System.out.println("沙漠骆驼！");
+        System.out.println("看我封魔！！");
+        System.out.println("中国男足！！");
+        System.out.println("如果感到快乐你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
