7a2bb11de093fefb4f37358cfaf27a28deb3e307
qiurunze <qiurunze@youxin.com>
2018-10-19 10:47:55 +0800

    测试冲突

--- src/main/java/com/geekq/design/TestSmartGit.java
+++ src/main/java/com/geekq/design/TestSmartGit.java
@@ -7,8 +7,8 @@ public class TestSmartGit {
         System.out.println("沙漠骆驼！");
         System.out.println("看我封魔！！");
         System.out.println("中国男足！！");
-        System.out.println("如果感到快乐你就拍拍手！！");
-        System.out.println("如果感到幸福你就拍拍手！！");
+        System.out.println("南庭新苑！！");
+        System.out.println("换房西路！！");
         System.out.println("如果感到幸福你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
